28a490b8c4fab336992854ae772500242ca202d7
Boonchu Imta <96614709+blooboy@users.noreply.github.com>
2025-02-23 21:23:58 +0700

    develop br commit chg1

---
+++ src/main/java/com/example/mynt/MyntApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyntApplication.class, args);
+	}
+
+}
